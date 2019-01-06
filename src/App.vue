b068694b5b974054e31c49d66f4cada09e2aa298
hyman <hyman@home.com>
2019-01-06 19:06:59 +0800

    首页的整体布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+
+    <el-container>
+      <el-header>头部</el-header>
+      <el-container>
+        <el-aside width="200px">侧边栏</el-aside>
+        <el-main>主要内容区</el-main>
+      </el-container>
+    </el-container>
+
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'App'
 }
+
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
